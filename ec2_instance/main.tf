32c6cc35760c68ad951a025eef374e0dbfe7a988
GeeksTechnology <139409059+GeeksTechnology@users.noreply.github.com>
2025-01-19 12:29:50 +0100

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = "kg-1.pem" 
+  subnet_id     = "subnet-0f9e12140b1838e6e"
   tags = {
     Name = var.instance_name
   }
